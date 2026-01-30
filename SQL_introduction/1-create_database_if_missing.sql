808a7910627eea08f4768a0a3e08b7b5ef4e6819
root <root@ip-10-42-32-221.ec2.internal>
2026-01-30 22:20:55 +0000

    Fix formatting and comments for SQL task

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,5 @@
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- Script to create the database hbtn_0c_0 if it does not exist
+
+-- Create the database only if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
