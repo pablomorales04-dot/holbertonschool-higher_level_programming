cfde7866a3fa2686a1a639c603405975e04fed47
root <root@ip-10-42-32-221.ec2.internal>
2026-01-31 00:14:58 +0000

    Convert hbtn_0c_0, first_table, and name field to UTF8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,7 @@
 -- Script to convert hbtn_0c_0 database, first_table, and its name field to UTF8
 
--- Convert the database to UTF8
+USE hbtn_0c_0;
+
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the column name to UTF8
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
